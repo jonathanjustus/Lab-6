a65489f7f91abdb17680aa664b8f7b89168385b9
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-02-23 18:29:53 -0600

    Cloned Lab 6 into GitHub

---
+++ Todo.java
@@ -0,0 +1,30 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-26
+OU ID_______________: 113480781
+OU EMAIL____________: jonathan.justus@ou.edu
+
+Tasks:
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+
+Estimated Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+
+Actual Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+*/}
