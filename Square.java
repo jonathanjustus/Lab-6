fba20bd0e22acd8ac49dd8dc0d4c1c6bcbac456b
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-02-26 20:24:40 -0600

    Complete Square Class

--- Square.java
+++ Square.java
@@ -1,5 +1,13 @@
 
-public class Square
+public class Square extends Rectangle
 {
-
+	public Square(String id, double size)
+	{	
+		super(id, size, size);	
+	}
+	
+	public String getShapeType()
+	{
+		return "Square";
+	}
 }
