2fd2858b356145473b2d97d2cd6f3de2ba41df93
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-02-26 20:26:11 -0600

    Complete Perimeter Comparator Class

--- ShapePerimeterComparator.java
+++ ShapePerimeterComparator.java
@@ -33,6 +33,20 @@ public class ShapePerimeterComparator implements Comparator<Shape>
 	public int compare(Shape s1, Shape s2)
 	{
 		// TODO: complete this...
+		if (s1.getPerimeter() > s2.getPerimeter())
+		{
+			return 1;
+		}
+		
+		else if (s1.getPerimeter() < s2.getPerimeter())
+		{
+			return -1;
+		}
+		
+		else
+		{
+			return 0;
+		}
 	}
 
 	/**
@@ -46,5 +60,8 @@ public class ShapePerimeterComparator implements Comparator<Shape>
 	public boolean equals(Shape s1, Shape s2)
 	{
 		// TODO: complete this...
+		int compareVal = compare(s1, s2);
+
+		return compareVal == 0;
 	}
 }
