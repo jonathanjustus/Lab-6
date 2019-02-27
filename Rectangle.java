2f803963bf59cf9a64c93969746946356e2703c8
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-02-26 20:24:09 -0600

    Complete the Rectangle Class

--- Rectangle.java
+++ Rectangle.java
@@ -1,5 +1,30 @@
 
-public class Rectangle
+public class Rectangle extends Polygon
 {
+	private double height;
+	private double width;
+	
+	public Rectangle(String id, double height, double width)
+	{
+		super(id);
+		this.height = height;
+		this.width = width;
+		
+		sideLengths.add(height);
+		sideLengths.add(height);
+		sideLengths.add(width);
+		sideLengths.add(width);
+	}
+	
+	public double getArea()
+	{
+		double area = height * width;
+		return area;
+	}
+	
+	public String getShapeType()
+	{
+		return "Rectangle";
+	}
 
 }
